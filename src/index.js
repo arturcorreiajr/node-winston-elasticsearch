0052d14971a72febf36c1a8fd67a0292fe1b6f54
arturcorreiajr <arturcorreiajr@gmail.com>
2022-08-04 18:51:09 -0300

    add APM agent

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
 
-
 const { createServer } = require('http')
 const winston = require('winston')
 const { ElasticsearchTransport } = require('winston-elasticsearch')
 const { Client } = require('@elastic/elasticsearch')
-
-
+const users = ['Tipscode', 'Dukeza', 'Tux']
 
 // Inicio - Lib apm-node
-//var apm = require('elastic-apm-node').start({
-//    serviceName: 'node-apm',
-    
-//    Set custom APM Server URL (default: http://localhost:8200)
-//    serverUrl: 'http://apm-server:8200'
-    
-//    })
-
+var apm = require('elastic-apm-node').start({
+    serviceName: 'server-apm',
+    serverUrl: 'http://apm-server.elastic.svc.cluster.local:8200',
+    environment: 'production'
+})
 
 // ESModules
 var esTransportOpts = {
@@ -24,7 +19,7 @@ var esTransportOpts = {
       auth: {
         username: "elastic",
         password: "t23lAi86dK8u1ve19ygI27vd"
-        }
+      }
     }
 
   }
@@ -36,12 +31,10 @@ const logger = winston.createLogger({
     winston.format.timestamp(), 
     winston.format.json()
 ),
-  defaultMeta: { service: 'user-service' },
+  defaultMeta: { service: 'test-service-APM', enviroment: 'elastic'},
   transports: [
-    //
     // - Write all logs with importance level of `error` or less to `error.log`
     // - Write all logs with importance level of `info` or less to `combined.log`
-    //
     new winston.transports.File({ filename: 'error.log', level: 'error' }),
     new winston.transports.File({ filename: 'all.log' }),
     esTransport
@@ -49,21 +42,38 @@ const logger = winston.createLogger({
   ],
 })
 
-
 //App Teste Node + Winston + Elastic + ApM
-
-
 class App {
   constructor() {
     this.server = createServer((req, res) => {
       // http://localhost:8080/stop -> Parar o servidor.
-        if (req.url === '/stop') {
+      if (req.url === '/stop') {
         res.writeHead(200, { 'Content-Type': 'application/json' })
         res.end(JSON.stringify({
-          message: 'Servidor parou'
-        }))
+        message: 'Servidor parou'
+      }))
         return this.stop()
       }
+      if (req.url === '/users') {
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify({
+        message: users
+      }))
+      return null
+      }
+      if (req.url === '/users/:index') {
+        const { index } = req.params
+        const { name } = req.body
+        users[index] = name
+
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify({
+        message: users
+      }))
+        return null
+      }
+
+
 // Resposta do servidor
       res.writeHead(200, { 'Content-Type': 'application/json' })
       res.end(JSON.stringify({
@@ -75,7 +85,7 @@ class App {
 //Iniciar o servidor
   
   start() {
-    logger.info('------LOG VINDO DO WINSTON-SANDBOX----!')
+    logger.info('[Log teste] - Aplicação teste APM Metrics!')
     this.server.listen(8080)
   }
 
@@ -89,6 +99,6 @@ class App {
 let app = new App()
 app.start()
 
-// docker build -t arturcorreiajr/node-winston:latest .
-// docker push arturcorreiajr/node-winston:latest 
+// docker build -t misolucoestech/node-winston-apm-v9:latest .
+// docker push misolucoestech/node-winston-apm-v9:latest 
 // kubectl apply -f yaml/node.yaml -n elastic 
